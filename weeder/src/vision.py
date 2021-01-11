f45273c7c68802cdc2d0a632e907217520fc339d
thememorydealer <mazvydas66@gmail.com>
2021-01-11 19:03:59 +0000

    got points in world coordinates

--- weeder/src/vision.py
+++ weeder/src/vision.py
@@ -1,21 +1,24 @@
 #! /usr/bin/env python
-import rospy
+import rospy, image_geometry, cv2, sys, tf
 from std_msgs.msg import String
-from sensor_msgs.msg import Image
-import cv2
+from sensor_msgs.msg import Image, CameraInfo, PointCloud
+from geometry_msgs.msg import PoseStamped
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-from colorama import Fore, Back, Style, init
-init(autoreset=True)
+from colorama import Fore, Back, Style, init # this allows for color print to console
+init(autoreset=True) # dont remember the color
 
 class Vision:
+    camera_model = None
     
     def __init__(self):
         self.sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect", Image, self.callback)
         self.difSub = rospy.Subscriber('/crop_difficulty', String, self.difCallback)
-        self.difSwitch = 'Hard'
+        self.difSwitch = 'Easy'
         self.pub = rospy.Publisher("/thorvald_001/kinect2_camera/hd/image_color_rect_filtered", Image, queue_size=1)
         self.bridge = CvBridge()
+        self.camera_info_sub = rospy.Subscriber('/thorvald_001/kinect2_camera/hd/camera_info', CameraInfo, self.cameraInfoCallback)
+        self.tf_listener = tf.TransformListener()
 
     def callback(self, msg): # callback should decide which crop bot is looking at
         try:
@@ -49,8 +52,67 @@ class Vision:
         # mask = cv2.dilate(mask, np.ones((19, 19)))
         cropimage = cv2.bitwise_and(HSVimage, HSVimage, mask=mask)
         image = cv2.cvtColor(cropimage, cv2.COLOR_HSV2BGR)
-        
         return image
+    
+    def detectWeeds(self, image, mask):
+        weedmask=cv2.bitwise_not(mask) # invert as blob detector will look for black pixels, ours is white
+        # STOLEN FROM -> https://www.learnopencv.com/blob-detection-using-opencv-python-c/
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByArea = True 
+        params.minArea = 250
+        params.maxArea = sys.maxint
+        # Filter by Circularity
+        params.filterByCircularity = True
+        params.minCircularity = -10
+        # Filter by Convexity
+        params.filterByConvexity = True
+        params.minConvexity = 0
+        # Filter by Inertia
+        params.filterByInertia = True
+        params.minInertiaRatio = 0
+        # Create a detector with the parameters
+        detector = cv2.SimpleBlobDetector_create(params)
+        # Detect blobs.
+        keypoints = detector.detect(weedmask)
+        # Draw detected blobs as red circles. cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+        im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        pointarray = []
+        for keyPoint in keypoints: # STOLEN FROM -> https://stackoverflow.com/a/37332802
+            x = int(keyPoint.pt[0])
+            y = int(keyPoint.pt[1])
+            s = keyPoint.size # diameter not area, might be useful later
+            print([int(x),int(y)])
+            a = self.pxToWorldFrame(int(x), int(y))
+            print(a)
+            # print(a.pose.position.x)
+            # print(a.pose.position.y)
+            # print(a.pose.position.z)
+            # pointarray.append([x,y])
+            # print(s)
+        # print(pointarray)
+        return im_with_keypoints
+    
+    def pxToWorldFrame(self, pxX, pxY):
+        # STOLEN FROM _> https://answers.ros.org/question/241624/converting-pixel-location-in-camera-frame-to-world-frame/?answer=242060#post-id-242060 + Greg's lab on img geometry
+        cam_model_point = self.camera_model.projectPixelTo3dRay(self.camera_model.rectifyPoint((pxX, pxY)))
+        bot_pose = PoseStamped()
+        bot_pose.pose.position.x = cam_model_point[0]
+        bot_pose.pose.position.y = cam_model_point[1]
+        bot_pose.pose.position.z = cam_model_point[2]
+        bot_pose.pose.orientation.x = 0
+        bot_pose.pose.orientation.y = 0
+        bot_pose.pose.orientation.z = 0
+        bot_pose.pose.orientation.w = 1.0
+        bot_pose.header.stamp = rospy.Time.now()
+        bot_pose.header.frame_id = self.camera_model.tfFrame()
+        self.tf_listener.waitForTransform(self.camera_model.tfFrame(), '/map', rospy.Time.now(), rospy.Duration(1.0))
+        tf_point = self.tf_listener.transformPose('/map', bot_pose)
+        return tf_point
+    
+    def cameraInfoCallback(self, data):
+        self.camera_model = image_geometry.PinholeCameraModel()
+        self.camera_model.fromCameraInfo(data)
+        self.camera_info_sub.unregister() #Only subscribe once
         
     def easyAlgo(self, image):
         imagenoground = self.removeGround(image)
@@ -62,43 +124,21 @@ class Vision:
         weedmask = cv2.inRange(HSVimage, min, max)
         # weedmask = cv2.erode(weedmask, kernelSize)
         weedmask = cv2.morphologyEx(weedmask, cv2.MORPH_OPEN, np.ones((8, 8)))
-        weedmask=cv2.bitwise_not(weedmask) # invert as blob detector will look for black, ours is white
         # mask out crop
         min1 = np.array([040, 130, 000]) 
         max1 = np.array([141, 206, 254]) 
         cropmask = cv2.inRange(HSVimage, min1, max1)
         cropimage = cv2.bitwise_and(HSVimage, HSVimage, mask=cropmask) 
         weedimage = cv2.bitwise_and(HSVimage, HSVimage, mask=weedmask) 
-        # image[np.all(image != (0, 0, 0), axis=-1)] = (179, 255, 255)
-        # STOLEN FROM -> https://www.learnopencv.com/blob-detection-using-opencv-python-c/
-        params = cv2.SimpleBlobDetector_Params()
-        params.filterByArea = False # false yet still need to be here otherwise default set to True
-        params.minArea = 100
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = -10
-        # Filter by Convexity
-        params.filterByConvexity = False
-        params.minConvexity = 0.87
-        # Filter by Inertia
-        params.filterByInertia = False
-        params.minInertiaRatio = 0.01
-        # Create a detector with the parameters
-        detector = cv2.SimpleBlobDetector_create(params)
-        # Detect blobs.
-        keypoints = detector.detect(weedmask)
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-        # the size of the circle corresponds to the size of blob
-        weedmask=cv2.bitwise_not(weedmask) # invert again to apply color 
-        # STOLEN FROM -> https://answers.opencv.org/question/100731/i-want-to-change-the-black-pixels-in-the-image-to-red/
+        # image[np.all(image != (0, 0, 0), axis=-1)] = (255, 255, 255) # turn all non black pixels white
+        # STOLEN FROM -> https://stackoverflow.com/a/60019059
         indices = np.where(weedmask==255)
         image[indices[0], indices[1], :] = [0, 0, 255] # weed turns red
         indicesgreen = np.where(cropmask==255)
         image[indicesgreen[0], indicesgreen[1], :] = [0, 255, 0] # crop turns green
-        im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         # cv2.imshow("filtered", im_with_keypoints)
         # cv2.waitKey(1)
+        im_with_keypoints = self.detectWeeds(image, weedmask)
         return im_with_keypoints
         
     def mediumAlgo(self, image):
@@ -129,28 +169,7 @@ class Vision:
         weedimage = cv2.cvtColor(weedimage, cv2.COLOR_HSV2BGR)
         indicesred = np.where(mask2==255)
         img_without_crop[indicesred[0], indicesred[1], :] = [0, 0, 255]
-        #  STOLEN FROM -> https://www.learnopencv.com/blob-detection-using-opencv-python-c/
-        params = cv2.SimpleBlobDetector_Params()
-        params.filterByArea = True 
-        params.minArea = 100
-        # Filter by Circularity
-        params.filterByCircularity = True
-        params.minCircularity = -1
-        # Filter by Convexity
-        params.filterByConvexity = False
-        params.minConvexity = 0.87
-        # Filter by Inertia
-        params.filterByInertia = True
-        params.minInertiaRatio = -1
-        # Create a detector with the parameters
-        detector = cv2.SimpleBlobDetector_create(params)
-        # Detect blobs.
-        mask2=cv2.bitwise_not(mask2)
-        keypoints = detector.detect(mask2)
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-        # the size of the circle corresponds to the size of blob        
-        im_with_keypoints = cv2.drawKeypoints(img_without_crop, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        im_with_keypoints = self.detectWeeds(img_without_crop, mask2)
         return im_with_keypoints
         
     def hardAlgo(self, image):
@@ -174,7 +193,7 @@ class Vision:
         mask = cv2.inRange(HSVimage, min, max)
         mask = cv2.dilate(mask, np.ones((19, 19)))
         
-        # STOLEN FROM -> https://stackoverflow.com/questions/42798659/how-to-remove-small-connected-objects-using-opencv
+        # STOLEN FROM -> https://stackoverflow.com/a/42812226
         dummy_image=mask.astype(np.uint8)
         #find all your connected components (white blobs in your image)
         nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(dummy_image, connectivity=8)
@@ -211,29 +230,7 @@ class Vision:
         indicesred = np.where(mask2==255)
         image[indicesred[0], indicesred[1], :] = [0, 0, 255]
         
-        mask2=cv2.bitwise_not(mask2)
-        # STOLEN FROM -> https://www.learnopencv.com/blob-detection-using-opencv-python-c/
-        params = cv2.SimpleBlobDetector_Params()
-        params.filterByArea = False # false yet still need to be here otherwise default set to True
-        params.minArea = 100
-        # Filter by Circularity
-        params.filterByCircularity = False
-        params.minCircularity = -10
-        # Filter by Convexity
-        params.filterByConvexity = False
-        params.minConvexity = 0.87
-        # Filter by Inertia
-        params.filterByInertia = False
-        params.minInertiaRatio = 0.01
-        # Create a detector with the parameters
-        detector = cv2.SimpleBlobDetector_create(params)
-        # Detect blobs.
-        keypoints = detector.detect(mask2)
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-        # the size of the circle corresponds to the size of blob
-        im_with_keypoints = cv2.drawKeypoints(image, keypoints, np.array([]), (255,255,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        
+        im_with_keypoints = self.detectWeeds(image, mask2)
         return im_with_keypoints
         
 
